b91141125fb7c88474020c5bfcdb310e97b492fd
DanitzaV <37908546+DanitzaV@users.noreply.github.com>
2018-10-06 01:21:12 -0300

    sort en ambos

--- src/componentes/Cartas.js
+++ src/componentes/Cartas.js
@@ -5,15 +5,24 @@ class Cartas extends Component {
     super();
     this.state = {
       cartas1: [1, 2, 3, 4, 5, 6],
-      cartas2: [1, 2, 3, 4, 5, 6]
+      cartas2: [6, 5, 2, 3, 1, 4]
     };
-    this.baraja = this.baraja.bind(this);
+    this.sort1 = this.sort1.bind(this);
+    this.sort2 = this.sort2.bind(this);
   }
   componentWillMount() {
-    this.baraja(this.state.cartas1);
+    this.sort1(this.state.cartas1);
+    this.sort2(this.state.cartas2);
     // this.baraja(this.state.cartas2);
   }
-  baraja(a) {
+  sort1(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+  sort2(a) {
     for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
@@ -24,7 +33,7 @@ class Cartas extends Component {
     return (
       <div>
         <ul>{this.state.cartas1.map(e => <li key={e.toString()}>{e}</li>)}</ul>
-        <ul>{this.state.cartas1.map(j => <li key={j.toString()}>{j}</li>)}</ul>
+        <ul>{this.state.cartas2.map(j => <li key={j.toString()}>{j}</li>)}</ul>
       </div>
     );
   }
